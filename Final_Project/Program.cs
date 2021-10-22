d2a165f91c2311850993ff607b92a3c8adcfe874
muradkerimov <muradik@code.edu.az>
2021-10-22 21:46:19 +0400

    Add project files.

---
+++ Final_Project/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Final_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+
+}
